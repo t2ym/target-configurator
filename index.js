f8576ed4b40f91ca202a4ee705fc5f1b022825f5
Tetsuya Mori <t2y3141592@gmail.com>
2020-09-10 18:57:28 +0900

    Correct LICENSE.md URL

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-@license https://github.com/t2ym/thin-hook/blob/master/LICENSE.md
+@license https://github.com/t2ym/target-configurator/blob/master/LICENSE.md
 Copyright (c) 2020 Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
 */
 const path = require('path');
